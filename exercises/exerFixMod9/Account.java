1e0dfe283124ba242bc3e085c2c8e35ce9d7a2fc
LucasConsani <lucasconsani.lc@gmail.com>
2024-03-15 22:18:06 -0300

    A new exercise done

---
+++ exercises/exerFixMod9/Account.java
@@ -0,0 +1,46 @@
+public class Account {
+
+    private int acNumber;
+    private String name;
+    private double value;
+
+    public Account(){
+
+    }
+    public Account(int acNumber, String name, double value){
+        this.acNumber = acNumber;
+        this.name = name;
+        this.value = value;
+    }
+
+    public Account(int acNumber, String name) {
+        this.acNumber = acNumber;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void depositValue(double deposit){
+        this.value += deposit;
+    }
+    public  void withdrawValue(double withdraw){
+        if(withdraw != 0) {
+            this.value -= withdraw + 5.0;
+        }
+    }
+
+    public String toString(){
+        return "Account: "
+                + acNumber
+                + ", Holder: "
+                + name
+                +String.format(", Balance: $ %.2f", value);
+    }
+
+
+}
---
+++ exercises/exerFixMod9/Main.java
@@ -0,0 +1,73 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter account number: ");
+        int acNumber = sc.nextInt();
+        System.out.print("Enter account holder: ");
+        sc.nextLine();
+        String name = sc.nextLine();
+        System.out.print("Is there an initial deposit (y/n)?");
+        char choose = sc.next().charAt(0);
+        if(choose == 'y') {
+            System.out.print("Enter initial deposit value: ");
+            double value = sc.nextDouble();
+            Account account = new Account(acNumber, name, value);
+            System.out.printf("%nAccount data:%n");
+            System.out.println(account);
+
+            System.out.printf("%nEnter a deposit value: ");
+            double deposit = sc.nextDouble();
+            account.depositValue(deposit);
+            System.out.println("Updated account data: ");
+            System.out.println(account);
+
+            System.out.printf("%nEnter a withdraw value: ");
+            double withdraw = sc.nextDouble();
+            account.withdrawValue(withdraw);
+            System.out.println("Update account data: ");
+            System.out.println(account);
+            System.out.println("Do you want to change your name (y/n)?");
+            choose = sc.next().charAt(0);
+            if(choose == 'y'){
+                System.out.print("Enter your name updated: ");
+                sc.nextLine();
+                String nameU = sc.nextLine();
+                account.setName(nameU);
+                System.out.println(account);
+            }else {
+                System.out.printf("Ok %s! Have a nice day!", account.getName());
+            }
+        }else{
+            Account account = new Account(acNumber, name);
+            System.out.printf("%nEnter a deposit value: ");
+            double deposit = sc.nextDouble();
+            account.depositValue(deposit);
+            System.out.println("Updated account data: ");
+            System.out.println(account);
+
+            System.out.printf("%nEnter a withdraw value: ");
+            double withdraw = sc.nextDouble();
+            account.withdrawValue(withdraw);
+            System.out.println("Update account data: ");
+            System.out.println(account);
+            System.out.println("Do you want to change your name (y/n)?");
+            choose = sc.next().charAt(0);
+            if(choose == 'y'){
+                System.out.print("Enter your name updated: ");
+                sc.nextLine();
+                String nameU = sc.nextLine();
+                account.setName(nameU);
+                System.out.println(account);
+            }else {
+                System.out.printf("Ok %s! Have a nice day!", account.getName());
+            }
+        }
+            sc.close();
+        }
+}
