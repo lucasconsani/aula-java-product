aa7ded38d0ac720aabce47bd542962296d3c9ad0
Lucas Guilherme Consani <lucasconsani.lc@gmail.com>
2024-03-22 10:20:48 -0300

    a new exercise resolved

---
+++ exercises/Mod9/exerFixMod9VectorExer2/Main.java
@@ -0,0 +1,39 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        double soma = 0.0;
+
+        System.out.println("Quantos números você irá digitar?");
+        int n = sc.nextInt();
+
+        double[] numero = new double[n];
+
+        for(int i = 0; i < numero.length; i++){
+            System.out.print("Digite um número: ");
+            numero[i] = sc.nextDouble();
+        }
+        System.out.print("VALORES: ");
+        for (double v : numero) {
+            System.out.printf("%.1f ",v);
+        }
+
+        for(double s : numero){
+            soma += s;
+        }
+        System.out.printf("%nSOMA: %.2f%n", soma);
+
+        double avg = soma / numero.length;
+        System.out.printf("MEDIA: %.2f", avg);
+
+
+
+        sc.close();
+
+    }
+}
