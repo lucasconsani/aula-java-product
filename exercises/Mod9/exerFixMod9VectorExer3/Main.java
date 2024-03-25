b15e7111e250c7f69450b5b3d3cae0922b9702df
Lucas Guilherme Consani <lucasconsani.lc@gmail.com>
2024-03-25 17:24:39 -0300

    a new exercise finished

--- exercises/Mod9/exerFixMod9VectorExer3/Main.java
+++ exercises/Mod9/exerFixMod9VectorExer3/Main.java
@@ -11,36 +11,46 @@ public class Main {
         System.out.println("Quantas pessoas serão digitadas?");
         int n = sc.nextInt();
 
-        int[] cont = new int[n];
         Pessoa[] pessoa = new Pessoa[n];
         double mediaAltura = 0.0;
-        int menorDe16 = 0;
+        int cont0 = 0;
+        int cont = 0;
 
-        for(int i : cont){
-            System.out.print("Nome: ");
+        for (int i = 0; i < pessoa.length; i++) {
+            cont += 1;
+            System.out.printf("%nDados da %d° pessoa:", cont);
+            System.out.printf("%nNome: ");
             sc.nextLine();
             String nome = sc.nextLine();
             System.out.print("Idade: ");
             int idade = sc.nextInt();
-            System.out.printf("%nAltura: ");
+            System.out.print("Altura: ");
             double altura = sc.nextDouble();
             pessoa[i] = new Pessoa(nome, idade, altura);
         }
 
-        for(int i : cont){
-            mediaAltura += pessoa[i].getAltura();
+
+        for (Pessoa value : pessoa) {
+            mediaAltura += value.getAltura();
         }
 
-        System.out.print("Altura média: " + mediaAltura / pessoa.length);
-        System.out.print("Pessoas com menos de 16 anos: ");
-        for(int j : cont){
-            menorDe16 = pessoa[j].getIdade();
-            if(menorDe16 < 16){
-
+        System.out.printf("%nAltura média: %.2f", mediaAltura / pessoa.length);
+        System.out.printf("%nPessoas com menos de 16 anos: ");
+        for (Pessoa value : pessoa) {
+            if (value.getIdade() < 16) {
+                cont0 += 1;
             }
         }
-
+        double porcentagem = (double) cont0 * 100 / pessoa.length;
+        System.out.printf("%.1f", porcentagem);
+        System.out.print("%");
+        for (Pessoa teste : pessoa) {
+            if (teste.getIdade() < 16) {
+                System.out.printf("%n%s ", teste.getNome());
+            }
+        }
 
         sc.close();
     }
 }
+
