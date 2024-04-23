95568aba267d30331b6bb3b948c4951ffa36fb3d
Lucas Consani <lucasconsani.lc@gmail.com>
2024-04-23 17:32:09 -0300

    A new exercise incomplete

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,48 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        Locale.setDefault(Locale.US);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Scanner sc = new Scanner(System.in);
 
-        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+        System.out.print("Enter department's name: ");
+        String dept = sc.next();
+        System.out.println("Enter Worker data: ");
+        System.out.println("Name: ");
+        sc.nextLine();
+        String name = sc.nextLine();
+        System.out.print("Level: ");
+        String level = sc.next();
+        System.out.print("Base Salary: ");
+        double baseSalary = sc.nextDouble();
+        Worker worker = new Worker(name, WorkerLevel.valueOf(level), baseSalary, new Department(dept));
+        System.out.println("How many contracts to this worker? ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            System.out.println("Enter contract #" + i + " data:");
+            System.out.print("Date (DD/MM/YYYY): ");
+            Date data = sdf.parse(sc.next());
+            System.out.print("Value per hour: ");
+            double value = sc.nextDouble();
+            System.out.print("Duration (hours): ");
+            int duration = sc.nextInt();
+            HourContract contract = new HourContract(data, value, duration);
+            worker.addContract(contract);
+        }
+        System.out.println();
+        System.out.print("Enter month and yeah to calculate income (MM/YYYY): ");
+        String monthAndYear = sc.next();
+        int month = Integer.parseInt(monthAndYear.substring(0, 2));
+        int year = Integer.parseInt(monthAndYear.substring(3));
 
-        System.out.println(order);
+        System.out.println("Name: " + worker.getName());
+        System.out.println("Department: " + worker.getDepartment().getName());
+        System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(month, year)));
 
-        OrderStatus os1 = OrderStatus.DELIVERED;
-        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
-
-        System.out.println(os1);
-        System.out.println(os2);
 
         sc.close();
     }
