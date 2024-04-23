bef12409ba847c3137d1e12d8bd170ad43320296
Lucas Consani <lucasconsani.lc@gmail.com>
2024-04-23 10:57:52 -0300

    new changes are included about the order archives

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
+import java.util.Date;
 import java.util.Scanner;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+
+        System.out.println(order);
+
+        OrderStatus os1 = OrderStatus.DELIVERED;
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+
+        System.out.println(os1);
+        System.out.println(os2);
 
         sc.close();
     }
